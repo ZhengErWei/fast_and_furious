dac2b226d734e95171c79b1e512479a3f587fd68
Andi Liao <31417269+liaoandi@users.noreply.github.com>
2018-05-26 14:45:02 -0500

    update time index

--- time_index/generate_timeindex.py
+++ time_index/generate_timeindex.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[25]:
+# In[5]:
 
 
 import pandas as pd
@@ -46,27 +46,34 @@ def add_month(row):
 def map_new_item(SAMPLE_DF):
     SAMPLE_DF['distance'] = SAMPLE_DF.apply(lambda x: calculate_distance(x['pickup_longitude'], x['pickup_latitude'], x['dropoff_longitude'], x['dropoff_latitude']), axis= 1)
     calculate_time(SAMPLE_DF)
-    SAMPLE_DF['unit'] = SAMPLE_DF['time_diff']/SAMPLE_DF['distance']
+    SAMPLE_DF['unit_time'] = SAMPLE_DF['time_diff']/SAMPLE_DF['distance']
+    SAMPLE_DF['unit_tip'] = SAMPLE_DF['tip_amount']/SAMPLE_DF['fare_amount']
     SAMPLE_DF['hour'] = SAMPLE_DF.apply(lambda x: add_hour(x), axis= 1)
     SAMPLE_DF['weekday'] = SAMPLE_DF.apply(lambda x: add_weekday(x), axis= 1)
     SAMPLE_DF['month'] = SAMPLE_DF.apply(lambda x: add_month(x), axis= 1)
-    SAMPLE_DF = SAMPLE_DF[(SAMPLE_DF['unit'] <= 100)]
+    SAMPLE_DF = SAMPLE_DF[(SAMPLE_DF['unit_time'] <= 100)]
     return SAMPLE_DF
 
     
-def normalize(column_name):
-    df = SAMPLE_DF['unit'].groupby(SAMPLE_DF[column_name]).mean()
+def normalize(column_name, standard_name):
+    df = SAMPLE_DF[standard_name].groupby(SAMPLE_DF[column_name]).mean()
     normalized_df=(df-df.min())/(df.max()-df.min())
     return normalized_df
 
 
 SAMPLE_DF = pd.read_csv('sample_trip.csv')
 SAMPLE_DF = map_new_item(SAMPLE_DF)
-hour_index = normalize('hour')
-weekday_index = normalize('weekday')
-month_index = normalize('month')
+hour_index_time = normalize('hour', 'unit_time')
+hour_index_tip = normalize('hour', 'unit_tip')
+weekday_index_time = normalize('weekday', 'unit_time')
+weekday_index_tip = normalize('weekday', 'unit_tip')
+month_index_time = normalize('month', 'unit_time')
+month_index_tip = normalize('month', 'unit_tip')
 
-hour_index.to_csv('hour_index.csv')
-weekday_index.to_csv('weekday_index.csv')
-month_index.to_csv('month_index.csv')
+hour_index_time.to_csv('hour_index_time.csv')
+hour_index_tip.to_csv('hour_index_tip.csv')
+weekday_index_time.to_csv('weekday_index_time.csv')
+weekday_index_tip.to_csv('weekday_index_tip.csv')
+month_index_time.to_csv('month_index_time.csv')
+month_index_tip.to_csv('month_index_tip.csv')
 
