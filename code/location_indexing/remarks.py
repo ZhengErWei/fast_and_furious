ee500239dbb4aea1193b745411308e37340bfccc
Wei Wei Zheng <weiweizheng@weiweiz.local>
2018-05-27 22:51:16 -0500

    dying

--- code/location_indexing/remarks.py
+++ code/location_indexing/remarks.py
@@ -12,8 +12,8 @@ coordinates = df[["pickup_longitude","pickup_latitude","dropoff_longitude","drop
 # filter = (coordinates['pickup_latitude'] <= -66.9513812) & (coordinates['pickup_longitude'] <= 49.3457868) \
 #             & (coordinates['dropoff_latitude'] <= -66.9513812) & (coordinates['pickup_longitude'] <= 49.3457868)
 # coordinates = coordinates[filter]
-coordinats1 = coordinates[["pickup_longitude","pickup_latitude"]]
-coordinats2 = coordinates[["dropoff_longitude","dropoff_latitude"]]
+coordinates1 = coordinates[["pickup_longitude","pickup_latitude"]]
+coordinates2 = coordinates[["dropoff_longitude","dropoff_latitude"]]
 coordinate_array1 = np.array(coordinates1)
 coordinate_array2 = np.array(coordinates2)
 kmeans1 = KMeans(n_clusters= 100, random_state=0).fit(coordinate_array1)
