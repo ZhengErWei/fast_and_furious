8e5c9fc80b81944f023b904815361eda2aaab4ee
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-06-01 11:16:10 -0500

    Add files via upload

--- code/matchregression.py
+++ code/matchregression.py
@@ -4,78 +4,118 @@ import math
 import csv
 
 
-class MRindex(MRJob):
+class MRpairreg(MRJob):
 
 
-	def mapper_init():
-		self.count = [0] * 6
-		self.x_mean = [0] * 6
-		self.y_mean = [0] * 6
-		self.x_sum = [0] * 6
-		self.y_sum = [0] * 6
-		self.beta_1 = [0] * 6
-		self.beta_0 = [0] * 6
+	def clean_output(self, row):
 
-		self.convert = dict(weather_st_ind:1, weather_end_ind:2, loc_ind:3, 
-							weekday_ind:4, hour_ind:5, month_ind: 6)
+		label, x1 = row[0].split(('\t['))
+		x1 = float(x1)
+		y1 = float(row[1].strip(' '))
+		x2 = float(row[2].strip(' '))
+		y2 = float(row[3].strip(' ]'))
+
+		return label, (x1, y1, x2, y2)
 
 
 	def mapper(self, _, line):
-		#need to change
-		key, original, compare = line.split(')') 
-
-		yield (key, (original, compare))
-
-
-	def reducer_init(self, key, value):
-		yield (key, value)
-
-
-	def reducer_first(self, key, value):
 		
+		row = next(csv.reader([line])) 
+		key, val = self.clean_output(row)
+		x_diff = val[0] - val[2]
+		y_diff = val[1] - val[3]
+		rv = (x_diff, y_diff)
+
+		yield key, rv
+
+
+
+	def reducer_first_init(self):
+
+		self.count = [0] * 6
+		self.x_sum = [0] * 6
+		self.y_sum = [0] * 6
+
+		self.convert = {'weather_st_ind':0, 'weather_end_ind':1, 'loc_ind':2, 
+						'weekday_ind':3, 'hour_ind':4, 'month_ind': 5}
+		self.data_dict = {}
+		self.x_mean = [0] * 6
+		self.y_mean = [0] * 6
+
+
+	def reducer_first(self, key, vals):
+		
+		print(key)
 		index = self.convert[key]
-		original, compare = value
-
-		x_diff = original[0] - compare[0]
-		y_diff = original[1] - compare[1]
-
-		self.x_sum[index] += x_diff
-		self.y_sum[index] += y_diff
-		self.count[index] += 1
-
-		yield (index, (x_diff, y_diff))
+		for val in vals:
+			x_diff, y_diff = val
+			self.x_sum[index] += x_diff
+			self.y_sum[index] += y_diff
+			self.count[index] += 1
 
 
-	def reducer_second(self):
+			if index not in self.data_dict:
+				self.data_dict[index] = []
+			self.data_dict[index].append((x_diff, y_diff))
+			# self.data_dict[index].append((x_diff, y_diff))
+			# yield index, (x_diff, y_diff)
+
+	def reducer_first_final(self):
 
 		self.x_mean = [a/b for a, b in zip(self.x_sum, self.count)]
 		self.y_mean = [a/b for a, b in zip(self.y_sum, self.count)]
+		
+
+		for key, values in self.data_dict.items():
+			for value in values:
+				x_diff, y_diff = value
+				sxy = (x_diff - self.x_mean[index]) * (y_diff - self.y_mean[index])
+				sxx = math.power((x_diff - self.x_mean[index]),2)
+				value = (sxy, sxx)
+				yield key, value
 
 
-	def reducer_sum(self, index, value):
 
-		x_diff, y_diff = value
+	def reducer_second_init(self):
 
-		top = (x_diff - self.x_mean[index]) * (y_diff - self.y_mean[index])
-		bottom = math.power((x_diff - self.x_mean[index]),2)
+		self.sxx = [0] * 6
+		self.sxy = [0] * 6
+		self.beta_1 = [0] * 6
+		self.beta_0 = [0] * 6
+		# self.x_mean = [a/b for a, b in zip(self.x_sum, self.count)]
+		# self.y_mean = [a/b for a, b in zip(self.y_sum, self.count)]
 
-		self.beta_1[index] += top / bottom
 
+	def reducer_second(self, index, vals):
 
-	def reducer_final(self):
+		for val in vals:
+			sxy, sxx = value
+			self.sxy[index] += sxy
+			self.sxx[index] += sxx
+
+	def reducer_second_final(self):
+
+		self.beta_1 = [a/b for a, b in zip(self.sxy, self.sxx)]
 		self.beta_0 = [a - b * c for a, b, c in zip(self.y_mean, self.x_mean, self.beta_1)]
 
-		yield (self.beta_0, self.beta_1)
+		for i in range(6):
+			yield 'beta_0', beta_0[i]
+			yield 'beta_1', beta_1[i]
 
 
 	def steps(self):
-    	return [MRStep(mapper_init = self.mapper_init, 
-    				   mapper = self.mapper, 
-    				   reducer = self.reducer_init),
-      			MRStep(reducer_init = self.reducer_first,
-      				   reducer = self.reducer_second,
-      				   reducer_final = self.reducer_final]
+
+		return [MRStep(mapper = self.mapper, 
+					   reducer_init = self.reducer_first_init,
+					   reducer = self.reducer_first,
+					   reducer_final = self.reducer_first_final),
+
+				MRStep(reducer_init = self.reducer_second_init,
+					   reducer = self.reducer_second,
+					   reducer_final = self.reducer_second_final)]
 
 
+if __name__ == '__main__':
+	MRpairreg.run()
 
 
