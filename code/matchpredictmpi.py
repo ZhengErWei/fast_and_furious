9daf3e713069418f3da0d9e6bcf975c59c405b6a
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-06-02 12:38:22 -0500

    Add files via upload

--- code/matchpredictmpi.py
+++ code/matchpredictmpi.py
@@ -1,13 +1,29 @@
 from mpi4py import MPI 
 import csv
 import mapindex_util
-import clean_output
 import numpy as np
 import sys
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
+
+
+def clean_raw_ind_data(row):
+
+	# ind_1 = row[0].strip("'[")
+	# ind_2 = row[1].strip(" '")
+	# ind_3 = row[2].strip(" '")
+	# ind_4 = row[3].strip(" '")
+	# ind_5 = row[4].strip(" '")
+	# ind_6, y = row[5].strip(" '").split(']\t')
+
+	ind_1, ind_2, ind_3, ind_4, ind_5, ind_6, y = row[1:]
+
+	rv = [ind_1, ind_2, ind_3, ind_4, ind_5, ind_6, y]
+	rv = [float(a) for a in rv]
+
+	return rv
 
 
 # scatter
