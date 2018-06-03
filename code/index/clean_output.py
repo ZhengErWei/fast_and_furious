5d0a140f189289f4dfab5d82611996093e8ef36c
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-06-03 01:36:51 -0500

    finished

---
+++ code/index/clean_output.py
@@ -0,0 +1,29 @@
+# python3 clean_output.py inputfile outputfile
+# use this file to clean the mapreduce result 
+# on location or 
+
+import csv
+import sys
+
+def clean_raw_sample_data(ipfile, opfile):
+
+	with open(ipfile, 'r') as f:
+
+		rb = csv.reader(f)
+		for row in rb:
+			cond = row[0].strip('["')
+			visi, val = row[1].strip(' "').split('"]\t')
+			with open(opfile, 'a') as f:
+
+				wb = csv.writer(f, delimiter=',')
+				wb.writerow([cond, visi, val])
+
+	return
+
+
+
+
+
+if __name__ == '__main__':
+ 	Input, Output = sys.argv[1:]
+	clean_raw_sample_data(Input, Output)
