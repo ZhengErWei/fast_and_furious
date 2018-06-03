efbe56d01726743079117e8749f4846ad06e77c4
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-06-03 15:08:23 -0500

    Update clean_output.py

--- code/index/clean_output.py
+++ code/index/clean_output.py
@@ -1,6 +1,7 @@
-# python3 clean_output.py inputfile outputfile
-# use this file to clean the mapreduce result 
-# on location or 
+# Purpose: use this file to clean the mapreduce result 
+#           on location or weather on sample_trip.csv
+# Command: python3 clean_output.py inputfile outputfile
+
 
 import csv
 import sys
