19de2b23c32a464bf62abc1cf23b08605475aff5
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-06-03 01:13:27 -0500

    finished

--- code/mapindex_util.py
+++ code/mapindex_util.py
@@ -1,23 +1,26 @@
+# include funcitons and datastructre to be included by matchpredictmpi.py
+# and others
 import pandas as pd 
 import sys
-sys.path.append('weather_indexing')
 import mapweather_util
-sys.path.append('location_indexing')
 import csv
 import pandas as pd
-import remarks
 import numpy as np
 from datetime import datetime
 import sample_y
+from math import sin, cos, sqrt, atan2, radians
+from sklearn.cluster import KMeans
+import numpy as np
 
 
 TIME_WEATHER_DICT = mapweather_util.get_weather_hour_dict()
 
-WEATHER_INDEX = pd.read_csv('../data/weather_index/weather_index_time.csv', header=None)
-LOCATION_INDEX = pd.read_csv('../data/location_index/location_index_time.csv', header=None)
-HOUR_INDEX = pd.read_csv('../data/time_index/hour_index_time.csv', header=None)
-MONTH_INDEX = pd.read_csv('../data/time_index/month_index_time.csv', header=None)
-WEEK_INDEX = pd.read_csv('../data/time_index/weekday_index_time.csv', header=None)
+# the files can be found in ../data
+WEATHER_INDEX = pd.read_csv('weather_index_time.csv', header=None)
+LOCATION_INDEX = pd.read_csv('location_index_time.csv', header=None)
+HOUR_INDEX = pd.read_csv('hour_index_time.csv', header=None)
+MONTH_INDEX = pd.read_csv('month_index_time.csv', header=None)
+WEEK_INDEX = pd.read_csv('weekday_index_time.csv', header=None)
 
 WEATHER_INDEX.columns = ['cond', 'visi', 'index']
 WEATHER_INDEX_DICT = {}
@@ -41,25 +44,35 @@ WEEK_INDEX.columns = ['day', 'index']
 WEEK_INDEX.set_index('day')
 
 
-KMEANS_1 = remarks.kmeans1
-KMEANS_2 = remarks.kmeans2
+
+# k means to get cluster label
+CLUSTER_DF = pd.read_csv('sample_trip.csv')
+COOR = CLUSTER_DF [["pickup_longitude","pickup_latitude","dropoff_longitude",
+				  	 	   "dropoff_latitude"]]
+COOR_1 = COOR[["pickup_longitude","pickup_latitude"]]
+COOR_2 = COOR[["dropoff_longitude","dropoff_latitude"]]
+COOR_ARRAY_1 = np.array(COOR_1)
+COOR_ARRAY_2 = np.array(COOR_2)
+KMEANS_1 = KMeans(n_clusters= 50, random_state=0).fit(COOR_ARRAY_1)
+KMEANS_2 = KMeans(n_clusters= 50, random_state=0).fit(COOR_ARRAY_2)
 
 
 
 def get_index(row):
 
-	start_date, start_hour = row[1].split(':')[0].split(' ')
+	start_date, start_hour = row[2].split(':')[0].split(' ')
 	start_hour = str(int(start_hour))
 	year, month, date = start_date.split('-')
 	start_date = ''.join([year, month, date])
-	end_date, end_hour = row[2].split(':')[0].split(' ')
+	end_date, end_hour = row[3].split(':')[0].split(' ')
 	end_date = ''.join(end_date.split('-'))
 	end_hour = str(int(end_hour))
 
-	pick_lon = float(row[5])
-	pick_lat = float(row[6])
-	drop_lon = float(row[9])
-	drop_lat = float(row[10])
+    # if work on larger dataset all index of row[] minus one
+	pick_lon = float(row[6])
+	pick_lat = float(row[7])
+	drop_lon = float(row[10])
+	drop_lat = float(row[11])
 
 	#weather index
 	start_weather_tup = (start_date, start_hour)
@@ -85,11 +98,15 @@ def get_index(row):
 
 	key = (weather_st_ind, weather_end_ind, loc_ind, weekday_ind, hour_ind, month_ind)
 
+	# for travel time
 	distance = sample_y.calculate_distance(pick_lat, pick_lon, drop_lat, drop_lat)
-	start_time = datetime.strptime(row[1], '%Y-%m-%d %H:%M:%S')
-	end_time = datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
+	start_time = datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
+	end_time = datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S')
 	time_diff = (end_time - start_time).total_seconds()/60
 	value = time_diff/distance
+
+	# for tip rate
+	# value = row[16]/row[13]
 
 	return key, value
 
