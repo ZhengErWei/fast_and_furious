0195a0e4c61aae8c012ac87e2f34562e77ba3a79
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-06-02 20:11:14 -0500

    finished

--- code/index/weather/mapweather.py
+++ code/index/weather/mapweather.py
@@ -1,5 +1,6 @@
 # map weather condition to the sample dataset/large dataset using mpi
 # can use file in ../../../data/sample.csv as the first input
+# command python3 thisfile inputfile(taxi trip) outputfile
 
 import mapweather_util
 from mpi4py import MPI 
