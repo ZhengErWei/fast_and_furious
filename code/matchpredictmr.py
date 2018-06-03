1ffebbbd728aab3c0712c02d6cf432a2302b71f3
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-06-03 01:53:46 -0500

    Update matchpredictmr.py

--- code/matchpredictmr.py
+++ code/matchpredictmr.py
@@ -1,3 +1,10 @@
+# use this file to get value of y of the most similar trip
+# after comparing each index of each row in a given file
+# and get the match with smallest differnce of each index
+
+# command: python3 thisfile inputdata
+# You can use sample_trip_2.csv in ../data
+
 from mrjob.job import MRJob
 import csv
 import pandas as pd 
@@ -10,9 +17,11 @@ from dateutil.rrule import rrule, DAILY, HOURLY
 
 class MRmatch(MRJob):
 
-
+	# same code as mapindex.py
+	# all data should be put in the same folder
 	def mapper_init(self):
 
+		# all data files should be put int the same folder
 		self.df = pd.read_csv('sample_trip.csv')
 		self.coordinates = self.df[["pickup_longitude", 
 							   		"pickup_latitude",
@@ -24,87 +33,92 @@ class MRmatch(MRJob):
 											  "dropoff_latitude"]]
 		self.coordinate_array1 = np.array(self.coordinates1)
 		self.coordinate_array2 = np.array(self.coordinates2)
-		self.KMEANS_1 = KMeans(n_clusters= 50, 
+		self.kmeans_1 = KMeans(n_clusters= 50, 
 							 random_state=0).fit(self.coordinate_array1)
-		self.KMEANS_2 = KMeans(n_clusters= 50, 
+		self.kmeans_2 = KMeans(n_clusters= 50, 
 							 random_state=0).fit(self.coordinate_array2)
 
-		self.WEATHER_DF = pd.read_csv('weather_201507_201606.csv')
-		self.WEATHER_DF.columns = ['date', 'hour', 'minute', 'visibility', 'cond']
-		self.WEATHER_DF['visibility'].replace(-9999, 10, inplace=True)
-		self.WEATHER_DF['date'] = self.WEATHER_DF['date'].astype(str)
-		self.WEATHER_DF['hour'] = self.WEATHER_DF['hour'].astype(str)
-		self.WEATHER_DF['minute'] = self.WEATHER_DF['minute'].astype(str)
+		self.weather_df = pd.read_csv('weather_201507_201606.csv')
+		self.weather_df.columns = ['date', 'hour', 'minute', 'visibility', 'cond']
+		self.weather_df['visibility'].replace(-9999, 10, inplace=True)
+		self.weather_df['date'] = self.weather_df['date'].astype(str)
+		self.weather_df['hour'] = self.weather_df['hour'].astype(str)
+		self.weather_df['minute'] = self.weather_df['minute'].astype(str)
 		for ind, row in self.WEATHER_DF.iterrows():
 			if row['cond'] == 'Unknown':
-				self.WEATHER_DF.loc[ind, 'cond'] = self.WEATHER_DF.iloc[ind - 1]['cond']
+				self.weather_df.loc[ind, 'cond'] = \
+				self.weather_df.iloc[ind - 1]['cond']
 
 
-		self.HOUR_LIST = []
-		self.START = date(2015, 7, 1)
-		self.END = date(2016, 6, 30)
-		for dt in rrule(HOURLY, dtstart = self.START, until = self.END):
-			self.HOUR_LIST.append(dt.strftime("%Y-%m-%d-%H"))
+		self.hour_list = []
+		self.start = date(2015, 7, 1)
+		self.end = date(2016, 6, 30)
+		for dt in rrule(HOURLY, dtstart = self.start, until = self.end):
+			self.hour_list.append(dt.strftime("%Y-%m-%d-%H"))
 
-		self.WEATHER_DICT = {}
-		for ind, row in self.WEATHER_DF.iterrows():
+		self.weather_dict = {}
+		for ind, row in self.weather_df.iterrows():
 			key = (row['date'], row['hour'])
 			value = (row['cond'], row['visibility'])
-			self.WEATHER_DICT[key] = self.WEATHER_DICT.get(key, value)
+			self.weather_dict[key] = self.weather_dict.get(key, value)
 
-		self.TIME_WEATHER_DICT = {}
-		for time in self.HOUR_LIST:
+		self.time_weather_dict = {}
+		for time in self.hour_list:
 			time_split = time.split('-')
 			day = ''.join(time_split[:3])
 			hour = str(int(time_split[-1]))
 			key = (day, hour)
-			if key not in self.WEATHER_DICT.keys():
+			if key not in self.weather_dict.keys():
 				new_key = self.get_last_hour(key)
-				while new_key not in self.WEATHER_DICT.keys():
+				while new_key not in self.weather_dict.keys():
 					new_key = self.get_last_hour(new_key)
-				self.TIME_WEATHER_DICT[key] = self.WEATHER_DICT[new_key]
+				self.time_weather_dict[key] = self.weather_dict[new_key]
 			else:
-				self.TIME_WEATHER_DICT[key] = self.WEATHER_DICT[key]
+				self.time_weather_dict[key] = self.weather_dict[key]
 
 
-		self.WEATHER_INDEX = pd.read_csv('weather_index_time.csv', header=None)
-		self.LOCATION_INDEX = pd.read_csv('location_index_time.csv', header=None)
-		self.HOUR_INDEX = pd.read_csv('hour_index_time.csv', header=None)
-		self.MONTH_INDEX = pd.read_csv('month_index_time.csv', header=None)
-		self.WEEK_INDEX = pd.read_csv('weekday_index_time.csv', header=None)
+		self.weather_index = pd.read_csv('weather_index_time.csv', header=None)
+		self.location_index = pd.read_csv('location_index_time.csv', header=None)
+		self.hour_index = pd.read_csv('hour_index_time.csv', header=None)
+		self.month_index = pd.read_csv('month_index_time.csv', header=None)
+		self.week_index = pd.read_csv('weekday_index_time.csv', header=None)
 
-		self.WEATHER_INDEX.columns = ['cond', 'visi', 'index']
-		self.WEATHER_INDEX_DICT = {}
-		for ind, row in self.WEATHER_INDEX.iterrows():
+		self.weather_index.columns = ['cond', 'visi', 'index']
+		self.weather_index_dict = {}
+		for ind, row in self.weather_index.iterrows():
 			key = (row['cond'], row['visi'])
 			value = row['index']
-			self.WEATHER_INDEX_DICT[key] = value
+			self.weather_index_dict[key] = value
 
-		self.LOCATION_INDEX.columns = ['pick', 'drop', 'index']
-		self.LOCATION_INDEX_DICT = {}
-		for ind, row in self.LOCATION_INDEX.iterrows():
+		self.location_index.columns = ['pick', 'drop', 'index']
+		self.location_index_dict = {}
+		for ind, row in self.location_index.iterrows():
 			key = (row['pick'], row['drop'])
 			value = row['index']
-			self.LOCATION_INDEX_DICT[key] = value
+			self.location_index_dict[key] = value
 
-		self.HOUR_INDEX.columns = ['hour', 'index']
-		self.HOUR_INDEX.set_index('hour')
-		self.MONTH_INDEX.columns = ['month', 'index']
-		self.MONTH_INDEX.set_index('month')
-		self.WEEK_INDEX.columns = ['day', 'index']
-		self.WEEK_INDEX.set_index('day')
+		self.hour_index.columns = ['hour', 'index']
+		self.hour_index.set_index('hour')
+		self.month_index.columns = ['month', 'index']
+		self.month_index.set_index('month')
+		self.week_index.columns = ['day', 'index']
+		self.week_index.set_index('day')
 
-		# self.filenames = ['raw_2015_08_time.csv']
-		# self.filenames = ['raw_sample_time_2.csv']
-		self.filenames = ['raw_time_total_1.csv']
+		# hard code here to choose the file you want to get the matching pair
+		self.filenames = ['raw_time_total_sample.csv', 'raw_time_total_sample_2.csv']
 
 		self.min_diff = [6] * 6
 
 
-
+	# same code as mapindex.py
 	def get_last_hour(self, key):
 		'''
-		input: ('20170101', '1')
+		Get the hour before the given hour
+
+		Input:
+			key: ('20170101', '1')
+
+		Return: tuple
 		'''
 
 		s = key[0] + '-' + key[1]
@@ -117,8 +131,13 @@ class MRmatch(MRJob):
 		return new_key
 
 
+	# this function copied from stackflow
 	def calculate_distance(self, lat1, lon1, lat2, lon2):
-	# approximate radius of earth in km
+		'''
+		Calculate the distance in mile between the given
+		longitude latitude between the start and end points
+		'''
+
 		R = 6373.0
 
 		dlon = radians(lon2) - radians(lon1)
@@ -132,57 +151,69 @@ class MRmatch(MRJob):
 		return distance
 
 
+	# same code as mapindex_util
 	def get_index(self, row):
 
-
-		start_date, start_hour = row[2].split(':')[0].split(' ')
+		# when deal with original taxi file all index after row plus one
+		start_date, start_hour = row[1].split(':')[0].split(' ')
 		start_hour = str(int(start_hour))
 		year, month, date = start_date.split('-')
 		start_date = ''.join([year, month, date])
-		end_date, end_hour = row[3].split(':')[0].split(' ')
+		end_date, end_hour = row[2].split(':')[0].split(' ')
 		end_date = ''.join(end_date.split('-'))
 		end_hour = str(int(end_hour))
 
-		pick_lon = float(row[6])
-		pick_lat = float(row[7])
-		drop_lon = float(row[10])
-		drop_lat = float(row[11])
+		pick_lon = float(row[5])
+		pick_lat = float(row[6])
+		drop_lon = float(row[9])
+		drop_lat = float(row[10])
 
 		#weather index
 		start_weather_tup = (start_date, start_hour)
-		start_cond = self.TIME_WEATHER_DICT[start_weather_tup]
-		weather_st_ind = self.WEATHER_INDEX_DICT[start_cond]
+		start_cond = self.time_weather_dict[start_weather_tup]
+		weather_st_ind = self.weather_index_dict[start_cond]
 
 		end_weather_tup = (end_date, end_hour)
-		end_cond = self.TIME_WEATHER_DICT[end_weather_tup]
-		weather_end_ind = self.WEATHER_INDEX_DICT[end_cond]
+		end_cond = self.time_weather_dict[end_weather_tup]
+		weather_end_ind = self.weather_index_dict[end_cond]
 
 		#location index
-		pick_clus = self.KMEANS_1.predict(np.array([pick_lon, pick_lat]).reshape(1,-1))
-		drop_clus = self.KMEANS_2.predict(np.array([drop_lon, drop_lat]).reshape(1,-1))
-		loc_ind = self.LOCATION_INDEX_DICT[(pick_clus[0], drop_clus[0])]
+		pick_clus = self.kmeans_1.predict(np.array([pick_lon, \
+													pick_lat]).reshape(1,-1))
+		drop_clus = self.kmeans_2.predict(np.array([drop_lon, \
+													drop_lat]).reshape(1,-1))
+		loc_ind = self.location_index_dict[(pick_clus[0], drop_clus[0])]
 
 		#time index
 		weekday = datetime.strptime(start_date, '%Y%m%d').weekday()
-		weekday_ind = self.WEEK_INDEX.loc[weekday]['index']
+		weekday_ind = self.week_index.loc[weekday]['index']
 
-		hour_ind = self.HOUR_INDEX.loc[int(start_hour)]['index']
+		hour_ind = self.hour_index.loc[int(start_hour)]['index']
 
-		month_ind = self.MONTH_INDEX.loc[int(month)]['index']
+		month_ind = self.month_index.loc[int(month)]['index']
 
-		key = (weather_st_ind, weather_end_ind, loc_ind, weekday_ind, hour_ind, month_ind)
+		key = (weather_st_ind, weather_end_ind, loc_ind, weekday_ind, \
+			   hour_ind, month_ind)
 
-		distance = self.calculate_distance(pick_lat, pick_lon, drop_lat, drop_lat)
+		# when y is traffic time
+		distance = self.calculate_distance(pick_lat, pick_lon, \
+										   drop_lat, drop_lat)
 		start_time = datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
 		end_time = datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S')
 		time_diff = (end_time - start_time).total_seconds()/60
-
 		value = time_diff/distance
+
+		# when y is tip rate
+		#value = float(row[15])/float(row[12])
 
 		return key, value
 
 
+
 	def clean_raw_ind_data(self, row):
+		'''
+		clean the output from mapreduce.
+		'''
 
 		# ind_1 = row[0].strip("'[")
 		# ind_2 = row[1].strip(" '")
@@ -200,12 +231,19 @@ class MRmatch(MRJob):
 
 
 	def get_diff(self, row1, row2):
+		'''
+		Get the difference of all index of two rows
+		'''
 
 		rv = [abs(i - j) for i, j in zip(row1, row2)]
 
 		return rv
 
 	def abs_smaller(self, list1, list2):
+		'''
+		To decide whether each entry of list2 
+		is smaller that that of list1
+		'''
 
 		for i in range(6):
 			if list1[i] > list2[i]:
@@ -214,6 +252,11 @@ class MRmatch(MRJob):
 		return True
 
 	def is_smallest(self, list1, list2, diff_standard):
+		'''
+		To decide whether each entrty of list2 is smaller
+		than list1, if yes, update the current minimum 
+		difference.
+		'''
 
 		diff = self.get_diff(list1, list2)
 		if self.abs_smaller(diff, diff_standard):
@@ -248,10 +291,9 @@ class MRmatch(MRJob):
 			except:
 				row = None
 
-
+	# same as mapper_init			
 	def reducer_init(self):
 
-		self.min_diff = [6] * 6
 		self.df = pd.read_csv('sample_trip.csv')
 		self.coordinates = self.df[["pickup_longitude", 
 							   		"pickup_latitude",
@@ -263,80 +305,85 @@ class MRmatch(MRJob):
 											  "dropoff_latitude"]]
 		self.coordinate_array1 = np.array(self.coordinates1)
 		self.coordinate_array2 = np.array(self.coordinates2)
-		self.KMEANS_1 = KMeans(n_clusters= 50, 
+		self.kmeans_1 = KMeans(n_clusters= 50, 
 							 random_state=0).fit(self.coordinate_array1)
-		self.KMEANS_2 = KMeans(n_clusters= 50, 
+		self.kmeans_2 = KMeans(n_clusters= 50, 
 							 random_state=0).fit(self.coordinate_array2)
 
-		self.WEATHER_DF = pd.read_csv('weather_201507_201606.csv')
-		self.WEATHER_DF.columns = ['date', 'hour', 'minute', 'visibility', 'cond']
-		self.WEATHER_DF['visibility'].replace(-9999, 10, inplace=True)
-		self.WEATHER_DF['date'] = self.WEATHER_DF['date'].astype(str)
-		self.WEATHER_DF['hour'] = self.WEATHER_DF['hour'].astype(str)
-		self.WEATHER_DF['minute'] = self.WEATHER_DF['minute'].astype(str)
+		self.weather_df = pd.read_csv('weather_201507_201606.csv')
+		self.weather_df.columns = ['date', 'hour', 'minute', 'visibility', 'cond']
+		self.weather_df['visibility'].replace(-9999, 10, inplace=True)
+		self.weather_df['date'] = self.weather_df['date'].astype(str)
+		self.weather_df['hour'] = self.weather_df['hour'].astype(str)
+		self.weather_df['minute'] = self.weather_df['minute'].astype(str)
 		for ind, row in self.WEATHER_DF.iterrows():
 			if row['cond'] == 'Unknown':
-				self.WEATHER_DF.loc[ind, 'cond'] = self.WEATHER_DF.iloc[ind - 1]['cond']
+				self.weather_df.loc[ind, 'cond'] = \
+				self.weather_df.iloc[ind - 1]['cond']
 
 
-		self.HOUR_LIST = []
-		self.START = date(2015, 7, 1)
-		self.END = date(2016, 6, 30)
-		for dt in rrule(HOURLY, dtstart = self.START, until = self.END):
-			self.HOUR_LIST.append(dt.strftime("%Y-%m-%d-%H"))
+		self.hour_list = []
+		self.start = date(2015, 7, 1)
+		self.end = date(2016, 6, 30)
+		for dt in rrule(HOURLY, dtstart = self.start, until = self.end):
+			self.hour_list.append(dt.strftime("%Y-%m-%d-%H"))
 
-		self.WEATHER_DICT = {}
-		for ind, row in self.WEATHER_DF.iterrows():
+		self.weather_dict = {}
+		for ind, row in self.weather_df.iterrows():
 			key = (row['date'], row['hour'])
 			value = (row['cond'], row['visibility'])
-			self.WEATHER_DICT[key] = self.WEATHER_DICT.get(key, value)
+			self.weather_dict[key] = self.weather_dict.get(key, value)
 
-		self.TIME_WEATHER_DICT = {}
-		for time in self.HOUR_LIST:
+		self.time_weather_dict = {}
+		for time in self.hour_list:
 			time_split = time.split('-')
 			day = ''.join(time_split[:3])
 			hour = str(int(time_split[-1]))
 			key = (day, hour)
-			if key not in self.WEATHER_DICT.keys():
+			if key not in self.weather_dict.keys():
 				new_key = self.get_last_hour(key)
-				while new_key not in self.WEATHER_DICT.keys():
+				while new_key not in self.weather_dict.keys():
 					new_key = self.get_last_hour(new_key)
-				self.TIME_WEATHER_DICT[key] = self.WEATHER_DICT[new_key]
+				self.time_weather_dict[key] = self.weather_dict[new_key]
 			else:
-				self.TIME_WEATHER_DICT[key] = self.WEATHER_DICT[key]
+				self.time_weather_dict[key] = self.weather_dict[key]
 
 
-		self.WEATHER_INDEX = pd.read_csv('weather_index_time.csv', header=None)
-		self.LOCATION_INDEX = pd.read_csv('location_index_time.csv', header=None)
-		self.HOUR_INDEX = pd.read_csv('hour_index_time.csv', header=None)
-		self.MONTH_INDEX = pd.read_csv('month_index_time.csv', header=None)
-		self.WEEK_INDEX = pd.read_csv('weekday_index_time.csv', header=None)
+		self.weather_index = pd.read_csv('weather_index_time.csv', header=None)
+		self.location_index = pd.read_csv('location_index_time.csv', header=None)
+		self.hour_index = pd.read_csv('hour_index_time.csv', header=None)
+		self.month_index = pd.read_csv('month_index_time.csv', header=None)
+		self.week_index = pd.read_csv('weekday_index_time.csv', header=None)
 
-		self.WEATHER_INDEX.columns = ['cond', 'visi', 'index']
-		self.WEATHER_INDEX_DICT = {}
-		for ind, row in self.WEATHER_INDEX.iterrows():
+		self.weather_index.columns = ['cond', 'visi', 'index']
+		self.weather_index_dict = {}
+		for ind, row in self.weather_index.iterrows():
 			key = (row['cond'], row['visi'])
 			value = row['index']
-			self.WEATHER_INDEX_DICT[key] = value
+			self.weather_index_dict[key] = value
 
-		self.LOCATION_INDEX.columns = ['pick', 'drop', 'index']
-		self.LOCATION_INDEX_DICT = {}
-		for ind, row in self.LOCATION_INDEX.iterrows():
+		self.location_index.columns = ['pick', 'drop', 'index']
+		self.location_index_dict = {}
+		for ind, row in self.location_index.iterrows():
 			key = (row['pick'], row['drop'])
 			value = row['index']
-			self.LOCATION_INDEX_DICT[key] = value
+			self.location_index_dict[key] = value
 
-		self.HOUR_INDEX.columns = ['hour', 'index']
-		self.HOUR_INDEX.set_index('hour')
-		self.MONTH_INDEX.columns = ['month', 'index']
-		self.MONTH_INDEX.set_index('month')
-		self.WEEK_INDEX.columns = ['day', 'index']
-		self.WEEK_INDEX.set_index('day')
+		self.hour_index.columns = ['hour', 'index']
+		self.hour_index.set_index('hour')
+		self.month_index.columns = ['month', 'index']
+		self.month_index.set_index('month')
+		self.week_index.columns = ['day', 'index']
+		self.week_index.set_index('day')
 
-		# self.filenames = ['raw_sample_time_2.csv']
-		self.filenames = ['raw_time_total_1.csv']
+		# hard code here to choose the file you want to get the matching pair
+		self.filenames = ['raw_time_total_sample.csv', 'raw_time_total_sample_2.csv']
+
+		self.min_diff = [6] * 6
 
 
+# yield each trip and pred y
+# output format see ../data/matchmr_sample.csv
 
 	def reducer(self, row, lists):
 
