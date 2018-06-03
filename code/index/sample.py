00ec83178c1205c5785e6bfb3fc427259d9cc58d
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-06-02 20:16:07 -0500

    Add files via upload

--- code/index/sample.py
+++ code/index/sample.py
@@ -1,7 +1,10 @@
-import pandas as pd 
-# # import map_weather_trip
-# import numpy as np
+# use this file to get sample file of given size from all taxi dataset
+# or index dataset
+# command: python3 thisfile
 
+import pandas as pd 
+
+# you can hard code here
 DIR = '../../data/'
 
 
@@ -9,13 +12,20 @@ DIR = '../../data/'
 def draw_one_sample(filename):
 
 	dirname = DIR + filename
-	print(dirname)
 	df = pd.read_csv(dirname)
 	df = df.dropna(how='any')
-	sample_df = df.sample(n = 2000000, random_state=34)
+	## for index file
+	df.iloc[:, 0] = df.iloc[:, 0].str.strip('[')
+	a = df.iloc[:,5].str.split(']\t',1).str[0]
+	b = df.iloc[:,5].str.split(']\t',1).str[1]
+	df_new = pd.concat([df.iloc[:,0], df.iloc[:,1], df.iloc[:,2], df.iloc[:,3], \
+						df.iloc[:,4], a, b], axis = 1, ignore_index = True)
+
+	sample_df = df_new.sample(n = 2000000, random_state=23)
 
 	return sample_df
 
+# taxi data names
 # FILENAMES = ['yellow_tripdata_2015-07.csv', 'yellow_tripdata_2015-08.csv', \
 # 			 'yellow_tripdata_2015-09.csv', 'yellow_tripdata_2015-10.csv', \
 # 			 'yellow_tripdata_2015-11.csv', 'yellow_tripdata_2015-12.csv', \
@@ -30,10 +40,11 @@ FILENAMES = ['raw_2015_07_time.csv', 'raw_2015_08_time.csv', 'raw_2015_09_time.c
 if __name__ == '__main__':
 	df_list = []
 	for file in FILENAMES:
-		print(file)
 		df_list.append(draw_one_sample(file))
 	rv = pd.concat(df_list)
-	rv.to_csv('../../data/raw_time_total_2.csv')
+
+	# you can hard code here
+	rv.to_csv('../../data/raw_time_total_1.csv')
 
 
 
