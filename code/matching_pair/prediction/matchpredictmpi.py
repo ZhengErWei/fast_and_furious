6c2552d476c93317963fb5c6c51c6b2a076499e6
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-06-03 23:36:28 -0500

    Update matchpredictmpi.py

--- code/matching_pair/prediction/matchpredictmpi.py
+++ code/matching_pair/prediction/matchpredictmpi.py
@@ -2,6 +2,7 @@
 # command: python3 thisfile
 # p.s. mapindex_util.py, mapweather_util.py and all files imported from these files should be put in the same folder
 # Input: files can be found in .../../../data (sample_trip_2.csv, raw_time_sample.csv)
+# Output: ../../../data/match_mr.txt
 
 from mpi4py import MPI 
 import csv
